feae23b8e3409f8c061a18e1c3b5eaf7ac4f3830
Threed95 <bolshanovad95@gmail.com>
2025-08-22 11:53:52 +0300

    создала массив arrWords

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const arrWords = [
+    { english: "hello", translation: "привет", example: "Hello, my name is Dasha!" },
+    { english: "dog", translation: "собака", example: "dog is man's best friend." },
+    { english: "hand", translation: "рука", example: "Give me your hand." },
+    { english: "night", translation: "ночь", example: "Good night, my friend." },
+    { english: "watermelon", translation: "арбуз", example: "This watermelon is very tasty." }
+]
+
+
 const flipCard = document.querySelector(".flip-card");
 
 const nextButton = document.querySelector("#next");
