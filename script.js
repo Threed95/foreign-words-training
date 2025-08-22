a1e5a833038a3c565ff4af0572415bfd3c45c4a4
Threed95 <bolshanovad95@gmail.com>
2025-08-22 23:34:45 +0300

    добавила функцию display card, обработчики событий на кнопки next и back

--- script.js
+++ script.js
@@ -6,14 +6,42 @@ const arrWords = [
     { english: "watermelon", translation: "арбуз", example: "This watermelon is very tasty." }
 ]
 
+let currentCardIndex = 0;
+
 
 const flipCard = document.querySelector(".flip-card");
+const titleCard = document.querySelector("#card-front h1");
+const cardBackTitle = document.querySelector("#card-back h1");
+const spanCardBack = document.querySelector("#card-back span");
 
 const nextButton = document.querySelector("#next");
 const backButton = document.querySelector("#back");
 
-flipCard.addEventListener("click", function() {
-    flipCard.classList.add("active");
+function displayCard(index) {
+    if (arrWords[index]) {
+        titleCard.textContent = arrWords[index].english;
+        cardBackTitle.textContent = arrWords[index].translation;
+        spanCardBack.textContent = arrWords[index].example;
+    }
+}
+
+nextButton.addEventListener("click", () => {
+    if (currentCardIndex < arrWords.length - 1) {
+        currentCardIndex++;
+        displayCard(currentCardIndex);
+    }
 })
 
-let currentIndex = 0;
+backButton.addEventListener("click", () => {
+    if (currentCardIndex > 0) {
+        currentCardIndex--;
+        displayCard(currentCardIndex);
+    }
+})
+
+displayCard(currentCardIndex);
+
+flipCard.addEventListener("click", function() {
+    flipCard.classList.add("active");
+
+})
